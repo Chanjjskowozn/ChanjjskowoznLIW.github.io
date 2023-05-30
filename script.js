57081b6bc8c0c871aefc4b8a6c21f3cd3dfb77a6
charity <134789806+Chanjjskowozn@users.noreply.github.com>
2023-05-30 18:53:41 +0800

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function age() {
+  var d1 = document.getElementById('date').value;
+
+
+  var daysPassed = d1 * 365;
+     var weeksPassed = d1 * 52;
+     var monthsPassed = d1 * 12;
+
+     //step b - Calculate days, weeks and months of limit age.
+     var daysLimit = 90 * 365;
+     var weeksLimit = 90 * 52;
+     var monthsLimit = 90 * 12;
+
+     //step c - subtract days, weeks and months of current and limit age.
+     var remainingDays = daysLimit - daysPassed;
+     var remainingWeeks = weeksLimit - weeksPassed;
+     var remainingMonths = monthsLimit - monthsPassed;
+
+  document.getElementById('age').innerHTML = 'You have '+remainingDays+' days '+remainingWeeks+' weeks, and '+remainingMonths+'months left';
+}function age() {
+  var d1 = document.getElementById('date').value;
+
+
+  var daysPassed = d1 * 365;
+     var weeksPassed = d1 * 52;
+     var monthsPassed = d1 * 12;
+
+     //step b - Calculate days, weeks and months of limit age.
+     var daysLimit = 90 * 365;
+     var weeksLimit = 90 * 52;
+     var monthsLimit = 90 * 12;
+
+     //step c - subtract days, weeks and months of current and limit age.
+     var remainingDays = daysLimit - daysPassed;
+     var remainingWeeks = weeksLimit - weeksPassed;
+     var remainingMonths = monthsLimit - monthsPassed;
+
+  document.getElementById('age').innerHTML = 'You have '+remainingDays+' days '+remainingWeeks+' weeks, and '+remainingMonths+'months left';
+}function age() {
+  var d1 = document.getElementById('date').value;
+
+
+  var daysPassed = d1 * 365;
+     var weeksPassed = d1 * 52;
+     var monthsPassed = d1 * 12;
+
+     //step b - Calculate days, weeks and months of limit age.
+     var daysLimit = 90 * 365;
+     var weeksLimit = 90 * 52;
+     var monthsLimit = 90 * 12;
+
+     //step c - subtract days, weeks and months of current and limit age.
+     var remainingDays = daysLimit - daysPassed;
+     var remainingWeeks = weeksLimit - weeksPassed;
+     var remainingMonths = monthsLimit - monthsPassed;
+
+  document.getElementById('age').innerHTML = 'You have '+remainingDays+' days '+remainingWeeks+' weeks, and '+remainingMonths+'months left';
+}
